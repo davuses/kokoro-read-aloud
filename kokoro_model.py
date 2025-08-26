e67dc9a68a74045ba662db9a8d54a6b05cada033
davuses <richardbaker3020@gmail.com>
2025-08-26 22:09:14 +0800

    update

--- kokoro_model.py
+++ kokoro_model.py
@@ -1,10 +1,15 @@
+import logging
+import time
 import warnings
 
 import numpy as np
-from kokoro import KModel, KPipeline
+from kokoro import KPipeline
 
 # Suppress all warnings
 warnings.filterwarnings("ignore")
+
+logging.basicConfig(level=logging.INFO)  # noqa: F821
+logger = logging.getLogger(__name__)
 
 
 def adjust_volume(audio: np.ndarray, gain: float) -> np.ndarray:
@@ -31,32 +36,30 @@ class KokoroModel:
 
     def __init__(self, lang_code="a"):
         # Initialize the TTS pipeline (only once)
-        kmodel = KModel(
-            config="model_config.json",
-            model="kokoro-v1_0.pth",
-            repo_id="hexgrad/Kokoro-82M",
-        )
+        import torch
+
+        if torch.cuda.is_available():
+            logger.info("GPU found, loading model on GPU")
         self.pipeline = KPipeline(
-            repo_id="hexgrad/Kokoro-82M",
-            lang_code=lang_code,
-            model=kmodel,
-            device="cuda",
-        )
+            lang_code=lang_code
+        )  # make sure lang_code matches voice
 
     def generate_audio(
         self, text: str, voice: str, speed=1, split_pattern=r"\n+"
     ):
         if voice not in self.ALLOWED_VOICES:
             voice = self.ALLOWED_VOICES[0]
+
         generator = self.pipeline(
             text, voice=voice, speed=speed, split_pattern=split_pattern
         )
-
         # Accumulate all audio chunks in a list
         audio_chunks = []
+        starttime = time.time()
         for i, (gs, ps, audio) in enumerate(generator):
             audio_chunks.append(audio)  # Add each audio chunk to the list
-
+        endtime = time.time()
+        logger.info(f"Time taken: {endtime - starttime:.2f} seconds")
         # Concatenate all audio chunks into one array
         if len(audio_chunks) > 0:
             audio_output = np.concatenate(audio_chunks)
